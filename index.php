cd706be3ad68be6e67e55a4d021242cf1da60d40
kupaqu <39209065+kupaqu@users.noreply.github.com>
2021-06-25 16:47:50 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <?php
 
 include_once('CurlRequest.php');
+include_once('download.php');
 set_time_limit(0);
 ini_set('memory_limit', '256M');
 
-$myCurlRequest = new CurlRequest('C:\Users\Buda\PhpstormProjects\parse\\' . date('Y-m-d-H-i-s') . '.json');
+$filename = date('Y-m-d-H-i-s') . '.json';
+
+$myCurlRequest = new CurlRequest('C:\Users\Buda\PhpstormProjects\parse\\' . $filename);
 
 // список стран https://namaztimes.kz/ru/api/country
 $countries = json_decode(file_get_contents('countries.json'), true);
@@ -13,3 +16,6 @@ $countries = json_decode(file_get_contents('countries.json'), true);
 foreach($countries as $country) {
     $myCurlRequest->putJSON(['proizvod' => $country]);
 }
+
+// отдаем файл на скачивание
+file_force_download($filename);
